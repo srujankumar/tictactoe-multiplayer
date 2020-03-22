55a29e1256728a9c48564fcf6b1f575879ef3806
Srujan Kumar Bojjam <srujan@swecha.net>
2020-03-22 18:36:55 +0530

    TicTacToe

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { Client } from "boardgame.io/react";
+import { SocketIO } from "boardgame.io/multiplayer";
+import { TicTacToe } from "./game";
+import { TicTacToeBoard } from "./board";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const TicTacToeClient = Client({
+    game: TicTacToe,
+    board: TicTacToeBoard,
+    debug: false,
+    multiplayer: SocketIO({ server: "localhost:8000" })
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    state = { playerID: null };
+
+    render() {
+        if (this.state.playerID === null) {
+            return (
+                <div>
+                    <p>Play as</p>
+                    <button onClick={() => this.setState({ playerID: "0" })}>
+                        Player 0
+                    </button>
+                    <button onClick={() => this.setState({ playerID: "1" })}>
+                        Player 1
+                    </button>
+                </div>
+            );
+        }
+        return (
+            <div>
+                <TicTacToeClient playerID={this.state.playerID} />
+            </div>
+        );
+    }
+}
+
+render(<App />, document.getElementById("root"));
